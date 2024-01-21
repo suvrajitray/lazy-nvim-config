3794682cc3a51df4c6578279a4b256a58d344c7b
Suvrajit Ray <suvrajitray@Suvrajits-Mac-mini.local>
2024-01-21 18:52:55 +0530

    comment line added as separator.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
+-------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------
 local keymap = vim.keymap
 keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find text (root dir)" })
