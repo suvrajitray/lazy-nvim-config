a40478649e2133fbe373a6844e93db339ad0081c
Suvrajit Ray <suray@microsoft.com>
2024-01-31 13:31:06 +0530

    disabled hardtime

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,3 +1,8 @@
+-- remove following line to enable the plugin
+if true then
+  return {}
+end
+
 return {
   "m4xshen/hardtime.nvim",
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
