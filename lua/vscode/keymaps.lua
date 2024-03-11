9550b09f9fd0ff8c55af52b447e0b280e2a4cbb3
Suvrajit Ray <suray@microsoft.com>
2024-03-11 12:56:06 +0530

    vscode setting added.

--- lua/vscode/keymaps.lua
+++ lua/vscode/keymaps.lua
@@ -1,8 +1,14 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+-- Better Navigation
+vim.api.nvim_set_keymap("n", "<C-j>", ':call VSCodeNotify("workbench.action.navigateDown")<CR>', { silent = true })
+vim.api.nvim_set_keymap("x", "<C-j>", ':call VSCodeNotify("workbench.action.navigateDown")<CR>', { silent = true })
+vim.api.nvim_set_keymap("n", "<C-k>", ':call VSCodeNotify("workbench.action.navigateUp")<CR>', { silent = true })
+vim.api.nvim_set_keymap("x", "<C-k>", ':call VSCodeNotify("workbench.action.navigateUp")<CR>', { silent = true })
+vim.api.nvim_set_keymap("n", "<C-h>", ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', { silent = true })
+vim.api.nvim_set_keymap("x", "<C-h>", ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', { silent = true })
+vim.api.nvim_set_keymap("n", "<C-l>", ':call VSCodeNotify("workbench.action.navigateRight")<CR>', { silent = true })
+vim.api.nvim_set_keymap("x", "<C-l>", ':call VSCodeNotify("workbench.action.navigateRight")<CR>', { silent = true })
 
-vim.cmd("nmap <leader>w :w<cr>")
-vim.cmd("nmap <leader>nh :nohl<cr>")
+vim.api.nvim_set_keymap("n", "gr", ':call VSCodeNotify("editor.action.goToReferences")<CR>', { silent = true })
 
 local keymap = vim.keymap
 
