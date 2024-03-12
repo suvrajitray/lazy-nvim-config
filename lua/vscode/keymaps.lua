5b30156815991e11b5b7300fb87fff2529b0052b
Suvrajit Ray <suray@microsoft.com>
2024-03-12 20:56:34 +0530

    vscode mapping added.

--- lua/vscode/keymaps.lua
+++ lua/vscode/keymaps.lua
@@ -11,20 +11,20 @@ function openWhichKeyInVisualMode()
 end
 
 -- Better Navigation
-vim.api.nvim_set_keymap("n", "<C-j>", ':call VSCodeNotify("workbench.action.navigateDown")<CR>', { silent = true })
-vim.api.nvim_set_keymap("x", "<C-j>", ':call VSCodeNotify("workbench.action.navigateDown")<CR>', { silent = true })
-vim.api.nvim_set_keymap("n", "<C-k>", ':call VSCodeNotify("workbench.action.navigateUp")<CR>', { silent = true })
-vim.api.nvim_set_keymap("x", "<C-k>", ':call VSCodeNotify("workbench.action.navigateUp")<CR>', { silent = true })
-vim.api.nvim_set_keymap("n", "<C-h>", ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', { silent = true })
-vim.api.nvim_set_keymap("x", "<C-h>", ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', { silent = true })
-vim.api.nvim_set_keymap("n", "<C-l>", ':call VSCodeNotify("workbench.action.navigateRight")<CR>', { silent = true })
-vim.api.nvim_set_keymap("x", "<C-l>", ':call VSCodeNotify("workbench.action.navigateRight")<CR>', { silent = true })
-
-vim.api.nvim_set_keymap("n", "<Space>", ':call VSCodeNotify("whichkey.show")<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "<Space>", ":call v:lua.openWhichKeyInVisualMode()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "gr", ':call VSCodeNotify("editor.action.goToReferences")<CR>', { silent = true })
-
 local keymap = vim.keymap
+
+keymap.set("n", "<C-j>", ':call VSCodeNotify("workbench.action.navigateDown")<CR>', { silent = true })
+keymap.set("x", "<C-j>", ':call VSCodeNotify("workbench.action.navigateDown")<CR>', { silent = true })
+keymap.set("n", "<C-k>", ':call VSCodeNotify("workbench.action.navigateUp")<CR>', { silent = true })
+keymap.set("x", "<C-k>", ':call VSCodeNotify("workbench.action.navigateUp")<CR>', { silent = true })
+keymap.set("n", "<C-h>", ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', { silent = true })
+keymap.set("x", "<C-h>", ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', { silent = true })
+keymap.set("n", "<C-l>", ':call VSCodeNotify("workbench.action.navigateRight")<CR>', { silent = true })
+keymap.set("x", "<C-l>", ':call VSCodeNotify("workbench.action.navigateRight")<CR>', { silent = true })
+
+keymap.set("n", "<Space>", ':call VSCodeNotify("whichkey.show")<CR>', { noremap = true, silent = true })
+keymap.set("x", "<Space>", ":call v:lua.openWhichKeyInVisualMode()<CR>", { noremap = true, silent = true })
+keymap.set("n", "gr", ':call VSCodeNotify("editor.action.goToReferences")<CR>', { silent = true })
 
 keymap.set({ "n", "x", "v" }, "<Up>", "5k", { desc = "smart vertical up" })
 keymap.set({ "n", "x", "v" }, "<Down>", "5j", { desc = "smart vertical down" })
